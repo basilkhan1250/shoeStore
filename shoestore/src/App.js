2a512272f10a4e66c09302381557eadc101464d8
M.BasilKhan <basilkhan1250@gmail.com>
2024-08-03 06:01:15 -0700

    working on it

--- shoestore/src/App.js
+++ shoestore/src/App.js
@@ -1,53 +1,9 @@
-import About from './About';
-import './App.css';
-import { Routes, Route, Link, useNavigate } from 'react-router-dom';
-import Home from './Home';
-import Products from './Products';
-import Id from './Id';
-import ProductDetails from './ProductDetails';
-import ProductHome from './ProductHome';
 
 
 function App() {
-  const Navigate = useNavigate()
   return (
     <div>
-      <h1>Hello World</h1>
-      <div>
-        <button>
-          <Link to={"/Home"} >Home</Link>
-        </button> {" "}
-
-        <button onClick={() => {
-          Navigate("/About")
-        }}>About</button>
-
-        {/* <button>
-          <Link to={"/About"} >About</Link>
-        </button>*/} {" "}
-        <button>
-          <Link to={"/Products"} >Products</Link>
-        </button> {" "}
-        <button>
-          <Link to={"/Id"} >Id</Link>
-        </button> {" "}
-        <button>
-          <Link to={"/Products/Mobile"} >Mobile</Link>
-        </button> {" "}
-        <button>
-          <Link to={"/Products/Laptop"} >Laptop</Link>
-        </button>
-      </div>
-
-      <Routes>
-        <Route path='/Home' element={<Home />} ></Route>
-        <Route path='/About' element={<About />} ></Route>
-        <Route path='/Products' element={<Products />} >
-          <Route path='' element={<ProductHome />} ></Route>
-          <Route path=':ProductId' element={<ProductDetails />} ></Route>
-        </Route>
-        <Route path='/Id' element={<Id />} ></Route>
-      </Routes>
+      
     </div>
   );
 }
